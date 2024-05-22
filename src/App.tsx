ed5a1942948891729bf09f57cda386ba27323761
budhil <budhilnigam@gmail.com>
2024-05-23 02:07:53 +0530

    Styling of both figures done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [player, setPlayer] = useState(1)
   const [tiles,setTiles] = useState([[0,0,0],[0,0,0],[0,0,0]]);
+  function handleClick(){
+  }
   return (
+    <>
+    <button onClick={()=>setPlayer((player+1)%2)}>Change player</button>
     <div id="container">
-      <span>
-      <button></button>
-      <button></button>
-      <button></button>
-      </span><span>
-      <button></button>
-      <button></button>
-      <button></button>
-      </span><span>
-      <button></button>
-      <button></button>
-      <button></button>
-      </span>
-      <div id="cross">
-        <div id ="line1"></div>
-        <div id ="line2"></div>
-      </div>
-      <div id="circle">
-
-      </div>
+      {tiles.map((tile,index)=>{
+        return <span key={'row'+index}>
+          {tile.map((t)=>
+          <button key={'tile'+t}>
+            {player==0?<div id='cross'>
+              <div id='line1'></div>
+              <div id='line2'></div>
+            </div>:
+            <div id='circle'>
+            </div>}
+          </button>)}
+        </span>
+      })}
     </div>
+    </>
   )
 }
 
